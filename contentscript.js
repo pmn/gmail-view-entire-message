58b998289104057db98f39a2a067ad500801123e
Paul Nakata <paul@pmn.org>
2012-05-23 09:25:11 -0700

    Cleanup

--- contentscript.js
+++ contentscript.js
@@ -9,6 +9,17 @@
 	}
     }
 
+    function testForClippedMessage(contentDoc){
+	// Google assigns the class 'vem' to the "View entire message" link
+	// at the bottom of clipped emails.
+	var vem = contentDoc.getElementsByClassName("vem");
+
+	if (vem.length > 0) {
+	    // Content contains the "View entire message" link, add the shortcut up top.
+	    insertViewMessageLink(contentDoc, vem[0]);
+	}
+    }
+
     function insertViewMessageLink(contentDoc, url){
 	// Currently Google gives the title bar a class of "ha".
 	// This may be an unreliable way to determine the attachment point.
@@ -23,21 +34,10 @@
 	vemLink.target = "_new";
 	vemLink.innerText = "View entire message";
 
-	// Make sure the link hasn't been inserted before:
+	// Make sure the link hasn't already been inserted:
 	if (! contentDoc.getElementById(elemId)) {
 	    // Insert the link
 	    titleBar[0].appendChild(vemLink);
-	}
-    }
-
-    function testForClippedMessage(contentDoc){
-	// Google assigns the class 'vem' to the "View entire message" link
-	// at the bottom of clipped emails.
-	var vem = contentDoc.getElementsByClassName("vem");
-
-	if (vem.length > 0) {
-	    // Content contains the "View entire message" link, add the shortcut up top.
-	    insertViewMessageLink(contentDoc, vem[0]);
 	}
     }
 
